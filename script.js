a4cf270dff57731c0f6634eaedaf5b901818953f
Muluken <mulerthegrete@gmail.com>
2021-10-27 12:52:10 -0400

    NA

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const showJokes = () =>{
 return fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
 .then(response => response.json())
 .then(quates =>{
-    console.log(quates.data.quoteText);
-    return jokes.textContent=quates.data.quoteText;
+let quate = quates.data;
+ quate.forEach(newQuate => {
+     jokes.textContent= newQuate.quoteText;
+ });
+
+    
 });
 
 }
