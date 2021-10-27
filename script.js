94ad8ffa7ea59d3c19f075df44b82eddd9e39da7
Muluken <mulerthegrete@gmail.com>
2021-10-27 12:33:08 -0400

    empty commit

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let jokes = document.getElementById("jokes")
 const showJokes = () =>{
 return fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
 .then(response => response.json())
-.then(data =>{
-    console.log(data.quote);
-    jokes.textContent=data.quote;
+.then(quates =>{
+    console.log(quates.data.quoteText);
+    return jokes.textContent=quates.data.quoteText;
 });
+
 }
